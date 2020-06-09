083c8d4e773bf7b2acabac6ed7105c960f6fc3ec
aniket <ani1998ket@gmail.com>
2020-06-09 20:41:09 +0530

    Added offset paramter for the string to be searched on

--- String_Matching/brute_string_match.cpp
+++ String_Matching/brute_string_match.cpp
@@ -3,43 +3,45 @@
 using std::string;
 
 /*
- *@author ani1998ket
+ * @author ani1998ket
  *
  * Matches pattern string within a text string
  * 
  * @param pattern String to be searched for
  * @param text String to be searched on
+ * @param pos Index of the `text` to start the search
  * @return index of first match, or len(`text`) if didn't match or `pattern` was empty
  */
-int brute_string_match( string const& pattern, string const& text );
+int brute_string_match( string const& pattern, string const& text, int pos = 0);
 
-int brute_string_match( string const& pattern, string const& text ){
+int brute_string_match( string const& pattern, string const& text, int pos){
     int p_len = pattern.size();
-    int t_len = text.size();
+    
+    //length of substring to be searched
+    int t_len = text.size() - pos;
     
     if( pattern == "" )
-        return t_len;
+        return text.size();
 
     if( t_len < p_len )
-        return t_len;
+        return text.size();
     
-    for( int i = 0; i < ( t_len - p_len + 1 ); i++ ){
+    for( int i = pos; i < ( text.size() - p_len + 1 ); i++ ){
          if( text.compare( i, p_len, pattern ) == 0 ){
             return i;
          }
     }
 
-    return t_len;
+    return text.size();
 }
 
 
 int main(){
-    string p = "aaaab";
-    string t = "aaaaaabaaaaabaaaaab";
+    string p = "aaaaab";
+    string t = "aaaaabaaaaaabaaabaaaaaabaaaab";
     int pos = brute_string_match( p, t );
     while( pos != t.size() ){
         std::cout << pos << std::endl;
-        pos++;
-        pos += brute_string_match( p, t.substr(pos) );
+        pos = brute_string_match( p, t, pos + 1 );
     }
 }
